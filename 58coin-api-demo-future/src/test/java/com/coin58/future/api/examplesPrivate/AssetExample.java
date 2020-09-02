27683c5a8c783b2f3c295b095ed98260b09aed94
jianweifan <jianweifan@58coin.com>
2020-09-02 11:02:06 +0800

    test

--- 58coin-api-demo-future/src/test/java/com/coin58/future/api/examplesPrivate/AssetExample.java
+++ 58coin-api-demo-future/src/test/java/com/coin58/future/api/examplesPrivate/AssetExample.java
@@ -14,8 +14,10 @@ import com.coin58.api.future.client.domain.AssetResponse;
  */
 public class AssetExample {
     public static void main(String[] args) {
-        String apiKey = "6616146f-6fe6-447c-bd31-c346437f201b";
-        String apiSecret = "E4199FA0CD19F4996A3D78E226ACAF2D";
+//        String apiKey = "6616146f-6fe6-447c-bd31-c346437f201b";
+//        String apiSecret = "E4199FA0CD19F4996A3D78E226ACAF2D";
+        String apiKey = "cc531d0d-5570-4c3a-985f-0bf0d050fbad";
+        String apiSecret = "534C96F7EE0236E11442AB852225DB20";
         ApiClientFactory factory = ApiClientFactory.newInstance(apiKey, apiSecret);
         ApiRestClient client = factory.newRestClient();
 
